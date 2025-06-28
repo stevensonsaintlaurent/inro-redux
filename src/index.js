c54b2431209e04a93178e36a999f5d9035cc4b97
Stevenson <stevensonstlaurent08@gmail.com>
2025-06-27 20:11:49 -0700

    react redux practicing component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store copy1";
+import { Provider } from "react-redux";
 
 store.dispatch({ type: "account/deposit", payload: 400 });
 console.log(store.getState());
@@ -10,6 +11,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
