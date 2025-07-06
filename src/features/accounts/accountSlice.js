04825d0aad38a3b319c5666f94be4c30a83f7aea
Stevenson <stevensonstlaurent08@gmail.com>
2025-07-06 09:17:19 -0700

    data fetch

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -34,8 +34,20 @@ export default function accountReducer(state = initialStateAccount, action) {
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    // API call
+
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    console.log(data);
+
+    // return action
+  };
 }
 
 export function withdraw(amount) {
